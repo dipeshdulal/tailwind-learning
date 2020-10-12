e29639e4b8d595bc7e8b5b7d0f0d8ba9a7c3145a
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-10-12 16:15:18 +0545

    feat: add hello world component

---
+++ src/Components/HelloWorld.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HelloWorld = () => {
+  return (
+    <div className="container mx-auto px-4 py-5 bg-blue-100">
+      <h1>Hello World</h1>
+    </div>
+  );
+};
