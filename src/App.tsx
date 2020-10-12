a3fd4848bf5dd323e44d40b60fa51304adf1c0e7
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2020-10-12 16:15:29 +0545

    feat: implement components

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import React from "react";
 import "./App.css";
 import "./tailwind.output.css";
 
+import { Card } from "./Components/Card";
+import { HelloWorld } from "./Components/HelloWorld";
+import { FlexBox } from "./Components/FlexBox";
+
 function App() {
   return (
     <div className="App">
-      <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
-        <div className="ml-6 pt-1">
-          <h1 className="text-2x1 text-blue-700 leading-tight">
-            Tailwind and Create React App
-          </h1>
-          <p className="text-base text-gray-700 loading-normal">
-            Building apps together
-          </p>
-        </div>
-      </div>
+      <Card />
+      <HelloWorld />
+      <FlexBox />
     </div>
   );
 }
